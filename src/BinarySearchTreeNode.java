94fee2d134bb8b1bf7ad6837a697f2aec463c059
Buildermark <Markhjuler@gmail.com>
2022-04-03 18:55:24 +0200

    Ikke nogle Fejl somså.

--- src/BinarySearchTreeNode.java
+++ src/BinarySearchTreeNode.java
@@ -1,10 +1,12 @@
 public class BinarySearchTreeNode<E extends Comparable> extends BinaryTreeNode<Number>
 {
     
-    private BinaryTreeNode leftChild, rightChild;
+    private BinaryTreeNode<Number> leftChild, rightChild;
     private E element;
-    public BinarySearchTreeNode(E element) {
-        super(element.compareTo(element));
+
+
+    public BinarySearchTreeNode(Number element) {
+        super(element);
         // TODO: 03-04-2022 Skal laves om vi compare til det samme element 
     }
 }
