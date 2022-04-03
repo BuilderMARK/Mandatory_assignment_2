df823a47175459e0fedd122df8c65c0c2b5e5774
Buildermark <Markhjuler@gmail.com>
2022-04-03 18:53:52 +0200

    Ikke nogle Fejl somså.

--- src/BinarySearchTreeNode.java
+++ src/BinarySearchTreeNode.java
@@ -1,8 +1,10 @@
-public class BinarySearchTreeNode<E extends Number> extends BinaryTreeNode<E>
+public class BinarySearchTreeNode<E extends Comparable> extends BinaryTreeNode<Number>
 {
+    
     private BinaryTreeNode leftChild, rightChild;
     private E element;
     public BinarySearchTreeNode(E element) {
-        super(element);
+        super(element.compareTo(element));
+        // TODO: 03-04-2022 Skal laves om vi compare til det samme element 
     }
 }
