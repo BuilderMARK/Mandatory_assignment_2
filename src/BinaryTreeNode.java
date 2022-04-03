b1a9182eef267085297582d25b827810c768a923
falken123321 <kasper.falk@live.dk>
2022-04-03 18:35:40 +0200

    binaryTreeNode done?

--- src/BinaryTreeNode.java
+++ src/BinaryTreeNode.java
@@ -1,4 +1,35 @@
-public class BinaryTreeNode {
+public class BinaryTreeNode<E extends Number> implements Comparable<BinaryTreeNode<E>> {
 
     private E element;
+    private BinaryTreeNode leftChild;
+    private BinaryTreeNode rightChild;
+
+    void setElement(E element) {
+        this.element = element;
+    }
+
+    public E getElement() {
+        return element;
+    }
+
+    void addLeftChild(BinaryTreeNode v) {
+        this.leftChild = v;
+    }
+
+    void addRightChild(BinaryTreeNode v) {
+        this.rightChild = v;
+    }
+
+    public BinaryTreeNode getLeftChild() {
+        return leftChild;
+    }
+
+    public BinaryTreeNode getRightChild() {
+        return rightChild;
+    }
+
+    @Override
+    public int compareTo(BinaryTreeNode<E> o) {
+        return 0;
+    }
 }
